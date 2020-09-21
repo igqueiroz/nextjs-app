a0bc507b2efe7b72f0390686c817dd2a4469c914
André T Gregório <andre.gregorio@rappi.com>
2020-09-21 13:57:58 -0300

    Add typescript

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Agenda 4 Pets</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Agenda 4 Pets</h1>
+    </div>
+  )
+}
